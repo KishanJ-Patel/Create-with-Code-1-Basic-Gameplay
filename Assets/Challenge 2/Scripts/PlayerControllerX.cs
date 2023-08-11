4ae417516850a26ea29312e4a0275f049bad0ebe
Kishan J Patel <kishan.patel2002@outlook.com>
2023-08-11 13:13:58 -0600

    Complete challenge 2 - play fetch

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    
+    [SerializeField] private float cooldown = 3.0f;
+
+    private float cooldownCount = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldownCount > 0) cooldownCount -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cooldownCount <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            cooldownCount = cooldown;
+        }
+    }
+}
